0beb032ed0a1a989cee02c1d352057275cae125e
Grayson Earle <graysonearle@gmail.com>
2023-11-13 17:03:47 +0100

    updates to arduino that arent working yet

--- arduino/working13nov/working13nov.ino
+++ arduino/working13nov/working13nov.ino
@@ -1,6 +1,6 @@
 #define SCANNER_PIN_FR 2
 #define SCANNER_PIN_AVI 3
-#define SCANNER_BRAKE_HEAD 4
+#define SCANNER_BRAKE_HOME 4
 #define TILT_FWD_PIN 5
 #define TILT_REV_PIN 6
 #define ROOMLIGHT_1_PIN 7
@@ -8,7 +8,7 @@
 #define DOORS_FWD_PIN 9
 #define DOORS_REV_PIN 10
 #define LEDS_PIN 11
-#define SCANNER_BRAKE_FEET 12
+#define SCANNER_BRAKE_AWAY 12
 
 #define BAUD_RATE 115200
 
@@ -18,19 +18,9 @@ long SCANNER_start_time = 0;
 // duration for speed 150 = 5000
 // duration for speed 200 = 4000
 int SCANNER_scan_duration = 4000;  // one full sweep is really 20 seconds
-bool SCANNER_move_to_head = false;
-bool SCANNER_move_to_feet = false;
-bool SCANNER_freakout = false;
-long SCANNER_freakout_start_time = 0;
-int SCANNER_freakout_step_duration = 2000;
-int SCANNER_freakout_max_duration = 4000;
-int SCANNER_freakout_step = 0;
-int SCANNER_freakout_num_steps = 8;
+bool SCANNER_forward = false;
+bool SCANNER_reverse = false;
 int SCANNER_speed = 90;
-bool SCANNER_override = false;  // when scanner hits a pre-stop, send it back to the middle before doing anything else with it
-int SO_direction = 0;           // scanner override direction (1 to head, 2 to feet)
-long SO_start_time = 0;         // when it started to override
-int SO_duration = 2000;         // override for X seconds
 
 // LEDs SCANNER
 #include <Adafruit_NeoPixel.h>
@@ -52,11 +42,11 @@ int doorSpeed = 255;
 void setup() {
 
   Serial.begin(BAUD_RATE);
-  Serial.setTimeout(10);
+  //Serial.setTimeout(2);
 
   // set up pins
   pinMode(SCANNER_PIN_FR, OUTPUT);
-  pinMode(SCANNER_BRAKE_HEAD, INPUT_PULLUP);
+  pinMode(SCANNER_BRAKE_HOME, INPUT_PULLUP);
   pinMode(ROOMLIGHT_1_PIN, OUTPUT);
   pinMode(ROOMLIGHT_2_PIN, OUTPUT);
   pinMode(TILT_FWD_PIN, OUTPUT);
@@ -66,29 +56,65 @@ void setup() {
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
 
   // set home positions
-  // scannerToFeetStart();
+  // scannerHome();
   // tiltHome();
 }
 
+void scannerHome() {
+
+  Serial.println("moving scanner to home position");
+  SCANNER_forward = true;
+  SCANNER_reverse = false;
+
+  analogWrite(SCANNER_PIN_AVI, SCANNER_speed);
+  digitalWrite(SCANNER_PIN_FR, HIGH);
+
+  bool home = false;
+  while (!home) {
+    if (scannerHitStart()) {
+      stopScan();
+      home = true;
+    }
+    delay(2);
+  }
+  Serial.println("scanner got to home, stopping home sequence");
+}
+
+void tiltHome() {
+
+  // I think the theory here is to tilt it to one direction for one minute (to be sure it's all the way),
+  // then to tilt it back half way (we have to base this on the time it takes since there is no sensor)
+
+  Serial.println("moving tilt to home position");
+
+  // tilt forward
+  handleTilt(1);
+
+  delay(TILT_DURATION);
+
+  // tild backward
+  handleTilt(2);
+  delay(TILT_DURATION / 2);
+
+  Serial.println("tilt motor got to home, stopping home sequence");
+}
+
+
 void loop() {
   // brake test
-  int brakeHead = digitalRead(SCANNER_BRAKE_HEAD);
-  if (brakeHead == LOW) {
-    Serial.println("scanner hit HEAD pre-stop");
-
-  }
-  int brakeFeet = digitalRead(SCANNER_BRAKE_FEET);
-  if (brakeFeet == LOW) {
-    Serial.println("scanner hit FEET pre-stop");
-    
+  int brk = digitalRead(SCANNER_BRAKE_HOME);
+  if (brk == LOW) {
+    Serial.println("braking");
   }
 
   if (Serial.available() > 0) {
     // read the incoming serial data until a newline character
-
+    
     String receivedString = Serial.readStringUntil('\n');
     Serial.print("received: ");
     Serial.println(receivedString);
+
+    
 
     // find the position of the comma character
     int commaIndex = receivedString.indexOf(',');
@@ -134,82 +160,17 @@ void loop() {
     }
   }
 
+
   // check on scanner
   scanner();
 
-  delay(2);
+  delay(1);
 }
-
-void scannerToFeetStart() {
-
-  Serial.println("starting to move scanner all the way to feet start position");
-  scannerTowardsFeet();
-
-  bool home = false;
-  while (!home) {
-    if (scannerHitFeet()) {
-      stopScan();
-      home = true;
-    }
-    delay(2);
-  }
-
-  // now move slightly towards head
-  Serial.println("scanner hit feet, moving slightly towards head");
-  scannerTowardsHead();
-  delay(2000);
-
-  Serial.println("scanner homing sequence complete");
-}
-
-void scannerTowardsFeet() {
-  Serial.println("moving scanner towards feet");
-  SCANNER_move_to_head = false;
-  SCANNER_move_to_feet = true;
-  analogWrite(SCANNER_PIN_AVI, SCANNER_speed);
-  digitalWrite(SCANNER_PIN_FR, HIGH);
-}
-
-void scannerTowardsHead() {
-  Serial.println("moving scanner towards head");
-  SCANNER_move_to_head = true;
-  SCANNER_move_to_feet = false;
-  analogWrite(SCANNER_PIN_AVI, SCANNER_speed);
-  digitalWrite(SCANNER_PIN_FR, HIGH);
-}
-
-void startScannerFreakout() {
-  SCANNER_freakout_start_time = millis();
-  SCANNER_freakout = true;
-  SCANNER_freakout_step = 1;
-}
-
-void tiltHome() {
-
-  // I think the theory here is to tilt it to one direction for one minute (to be sure it's all the way),
-  // then to tilt it back half way (we have to base this on the time it takes since there is no sensor)
-
-  Serial.println("moving tilt to home position");
-
-  // tilt forward (towards feet)
-  handleTilt(1);
-
-  delay(TILT_DURATION);
-
-  // tild backward
-  handleTilt(2);
-  delay(TILT_DURATION / 2);
-
-  Serial.println("tilt motor got to home, stopping home sequence");
-}
-
 
 void handleLEDs(int value) {
   // 0 = off
   // 1 = blue
   // 2 = red
-  // 3 =
-  // 4 =
   switch (value) {
     case 0:
       setAllLeds(0, 0, 0, 0);
@@ -227,9 +188,6 @@ void handleLEDs(int value) {
 }
 
 void handleTilt(int value) {
-  // 0: stop
-  // 1: tilt forward
-  // 2: tilt backward
   switch (value) {
     case 0:
       analogWrite(TILT_FWD_PIN, 0);
@@ -239,11 +197,13 @@ void handleTilt(int value) {
     case 1:  // Forward rotation
       analogWrite(TILT_FWD_PIN, tiltSpeed);
       analogWrite(TILT_REV_PIN, 0);
+
       break;
 
     case 2:
       analogWrite(TILT_FWD_PIN, 0);
       analogWrite(TILT_REV_PIN, tiltSpeed);
+
       break;
   }
 }
@@ -255,14 +215,16 @@ void handleDoors(int value) {
       analogWrite(DOORS_REV_PIN, 0);
       break;
 
-    case 1:  // doors close
+    case 1:  // Forward rotation
       analogWrite(DOORS_FWD_PIN, doorSpeed);
       analogWrite(DOORS_REV_PIN, 0);
+
       break;
 
-    case 2:  // doors open
+    case 2:
       analogWrite(DOORS_FWD_PIN, 0);
       analogWrite(DOORS_REV_PIN, doorSpeed);
+
       break;
   }
 }
@@ -271,13 +233,10 @@ void handleScanner(int value) {
   if (value == 0) {
     stopScan();
   } else if (value == 1) {
-    startFullScan();
-  } else if (value == 2) {
-    startScannerFreakout();
+    startScan();
   }
 }
 
-// room lights turn on and off via relay
 void handleRoomLights(int lights, int value) {
 
   int pin = (lights == 1) ? ROOMLIGHT_1_PIN : ROOMLIGHT_2_PIN;
@@ -289,7 +248,6 @@ void handleRoomLights(int lights, int value) {
   }
 }
 
-// helper to turn all LEDs a certain color
 void setAllLeds(int r, int g, int b, int brightness) {
   for (int i = 0; i < pixels.numPixels(); i++) {
     pixels.setPixelColor(i, pixels.Color(r, g, b));
@@ -298,11 +256,10 @@ void setAllLeds(int r, int g, int b, int brightness) {
   pixels.show();
 }
 
-// start going from feet to head and back
-void startFullScan() {
-  Serial.println("starting full scan");
-  SCANNER_move_to_head = true;
-  SCANNER_move_to_feet = false;
+void startScan() {
+  Serial.println("starting scan");
+  SCANNER_forward = true;
+  SCANNER_reverse = false;
 
   analogWrite(SCANNER_PIN_AVI, SCANNER_speed);
   digitalWrite(SCANNER_PIN_FR, HIGH);
@@ -310,76 +267,46 @@ void startFullScan() {
   SCANNER_start_time = millis();
 }
 
-// stop scanner motor
 void stopScan() {
   Serial.println("stopping scan");
   analogWrite(SCANNER_PIN_AVI, 0);
   digitalWrite(SCANNER_PIN_FR, LOW);
 }
 
-// check if scanner hit head pre-stop
-bool scannerHitHead() {
+bool scannerHitStart() {
   // check on scanner hitting the ends
-  if (digitalRead(SCANNER_BRAKE_HEAD) == LOW) {
+  if (digitalRead(SCANNER_BRAKE_HOME) == LOW) {
     return true;
   } else {
     return false;
   }
 }
 
-// check if scanner hit feet pre-stop
-bool scannerHitFeet() {
+bool scannerHitEnd() {
   // check on scanner hitting the ends
-  if (digitalRead(SCANNER_BRAKE_FEET) == LOW) {
-    return true;
-  } else {
-    return false;
-  }
+  return false;
 }
 
 void scanner() {
 
-  if (scannerHitFeet()) {
-    SCANNER_override = true;
-    SO_direction = 1;  // move towards head position
-    Serial.println("scanner hit feet, moving towards head slightly");
+  if (scannerHitStart()) {
+    stopScan();
   }
-
-  if (scannerHitHead()) {
-    SCANNER_override = true;
-    SO_direction = 2;  // move towards head position
-    Serial.println("scanner hit head, moving towards feet slightly");
+  if (scannerHitEnd()) {
+    stopScan();
   }
 
   long currentTime = millis();
 
-  if (SCANNER_freakout) {
-    if (currentTime > SCANNER_freakout_start_time + SCANNER_freakout_step_duration) {
-      SCANNER_freakout_start_time = millis();
-      SCANNER_freakout_step++;
-
-      // even steps have half duration
-      if (SCANNER_freakout_step % 2 == 0) {
-        SCANNER_freakout_step_duration = SCANNER_freakout_max_duration / 2;
-      } else {
-        SCANNER_freakout_step_duration = SCANNER_freakout_max_duration;
-      }
-
-      if(SCANNER_freakout_step >= SCANNER_freakout_num_steps) {
-        SCANNER_freakout = false;
-      }
-    }
-  }
-
-  if (SCANNER_move_to_head || SCANNER_move_to_feet) {
+  if (SCANNER_forward || SCANNER_reverse) {
 
     if (currentTime > SCANNER_start_time + SCANNER_scan_duration) {
 
       Serial.println("Scanner timer went off");
 
-      if (SCANNER_move_to_head) {
-        SCANNER_move_to_head = false;
-        SCANNER_move_to_feet = true;
+      if (SCANNER_forward) {
+        SCANNER_forward = false;
+        SCANNER_reverse = true;
         SCANNER_start_time = millis();
 
         // switch to reverse
@@ -387,9 +314,9 @@ void scanner() {
         analogWrite(SCANNER_PIN_AVI, SCANNER_speed);
 
         Serial.println("scan has gone all the way forward, returning");
-      } else if (SCANNER_move_to_feet) {
-        SCANNER_move_to_head = false;
-        SCANNER_move_to_feet = false;
+      } else if (SCANNER_reverse) {
+        SCANNER_forward = false;
+        SCANNER_reverse = false;
         // switch to forward (for later)
         digitalWrite(SCANNER_PIN_FR, HIGH);
         // stop it
